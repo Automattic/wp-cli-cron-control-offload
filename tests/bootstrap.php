2bbbfbab0d1d311b18c5df6cdfd4a9589b2e39e6
Erick Hitter <git-contrib@ethitter.com>
2017-09-07 15:52:36 -0700

    More renaming

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-cli-via-cron-control.php';
+	require dirname( dirname( __FILE__ ) ) . '/wp-cli-cron-control-offload.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
