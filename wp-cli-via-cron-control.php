6d7c4f5f9008b5a4160f0012e3bf0ba7e1388a25
Erick Hitter <erick@ethitter.com>
2017-09-07 21:42:34 +0000

    Plugin scaffold care of WP-CLI

---
+++ wp-cli-via-cron-control.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Cli Via Cron Control
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-cli-via-cron-control
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Cli_Via_Cron_Control
+ */
