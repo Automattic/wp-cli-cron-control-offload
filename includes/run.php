626c3f7c36e19e5a71699b329f6fba225aa81525
Erick Hitter <git-contrib@ethitter.com>
2017-09-13 19:26:43 -0700

    Use actions so users can decide how to handle "output"

--- includes/run.php
+++ includes/run.php
@@ -7,6 +7,7 @@
 
 namespace Automattic\WP\WP_CLI_Cron_Control_Offload;
 use WP_CLI;
+use WP_Error;
 
 /**
  * Intended for non-interactive use, so all output ends up in the error log
@@ -16,14 +17,14 @@ use WP_CLI;
  */
 function run_event( $command ) {
 	if ( ! defined( 'WP_CLI' ) || ! \WP_CLI ) {
-		/* translators: 1: Plugin's prefix for log messages, 2. WP-CLI command that would have run */
-		trigger_error( sprintf( __( '%1$s: Attempted to run event without WP-CLI loaded. (%2$s)', 'wp-cli-cron-control-offload' ), MESSAGE_PREFIX, var_export( $command, true ) ), E_USER_WARNING );
+		$no_wp_cli = new WP_Error( 'no-wp-cli', __( 'Attempted to run event without WP-CLI loaded.', 'wp-cli-cron-control-offload' ) );
+		do_action( 'wp_cli_cron_control_offload_run_error', $no_wp_cli, $command );
 		return;
 	}
 
-	if ( ! validate_command( $command ) ) {
-		/* translators: 1: Plugin's prefix for log messages, 2. WP-CLI command that would have run */
-		trigger_error( sprintf( __( '%1$s: Attempted to run blocked command. (%2$s)', 'wp-cli-cron-control-offload' ), MESSAGE_PREFIX, var_export( $command, true ) ), E_USER_WARNING );
+	$validated = validate_command( $command );
+	if ( is_wp_error( $validated ) ) {
+		do_action( 'wp_cli_cron_control_offload_run_error', $validated, $command );
 		return;
 	}
 
@@ -38,12 +39,12 @@ function run_event( $command ) {
 	$end = microtime( true );
 
 	// Command failed.
-	if ( ! is_object( $output ) || is_wp_error( $output ) ) {
-		/* translators: 1: Plugin's prefix for log messages, 2. Command error that caused failure */
-		trigger_error( sprintf( __( '%1$s: Command failed. (%2$s)', 'wp-cli-cron-control-offload' ), MESSAGE_PREFIX, var_export( $command, true ) ), E_USER_WARNING );
-
-		$message = is_wp_error( $output ) ? $output->get_error_message() : var_export( $output, true );
-		trigger_error( $message, E_USER_WARNING );
+	if ( is_wp_error( $output ) ) {
+		do_action( 'wp_cli_cron_control_offload_run_error', $output, $command );
+		return;
+	} elseif ( ! is_object( $output ) ) {
+		$error = new WP_Error( 'command-run-unknown-failure', __( 'Command execution failed with an unexpected error.', 'wp-cli-cron-control-offload' ), $output );
+		do_action( 'wp_cli_cron_control_offload_run_error', $error, $command );
 		return;
 	}
 
@@ -53,9 +54,6 @@ function run_event( $command ) {
 	$output->end      = $end;
 	$output->duration = $end - $start;
 
-	$output = var_export( $output, true );
-	$output = MESSAGE_PREFIX . ":\n{$output}";
-
-	error_log( $output );
+	do_action( 'wp_cli_cron_control_offload_run_success', $output, $command );
 }
 add_action( ACTION, __NAMESPACE__ . '\run_event' );
